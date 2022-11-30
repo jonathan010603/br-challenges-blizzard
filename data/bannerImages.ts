fa3abcd2043044cada7741203178d436bfb2eb47
jonathan010603 <jonathan010603@gmail.com>
2022-11-30 17:51:19 -0300

    feat (sections and header): basic layout
    - Divided the page layout in sections
    - Added background to banner section
    - Added gradient to background
    - Added header

---
+++ data/bannerImages.ts
@@ -0,0 +1,20 @@
+export const bannerImages = [
+  {
+    animationCover: "/assets/banner-hero/games/diablo-animation-cover.png",
+    animationGif: "/assets/banner-hero/games/diablo-animation.gif",
+    logo: "/assets/banner-hero/games/diablo-logo.png",
+    bg: "/assets/banner-hero/games/diablo-bg.png",
+  },
+  {
+    animationCover: "/assets/banner-hero/games/hearthstone-animation-cover.png",
+    animationGif: "/assets/banner-hero/games/hearthstone-animation.gif",
+    logo: "/assets/banner-hero/games/hearthstone-logo.png",
+    bg: "/assets/banner-hero/games/hearthstone-bg.png",
+  },
+  {
+    animationCover: "/assets/banner-hero/games/wow-animation-cover.png",
+    animationGif: "/assets/banner-hero/games/wow-animation.gif",
+    logo: "/assets/banner-hero/games/wow-logo.png",
+    bg: "/assets/banner-hero/games/wow-bg.png",
+  },
+];
