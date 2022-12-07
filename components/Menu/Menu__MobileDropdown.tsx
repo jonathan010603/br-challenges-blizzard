ec717c8428c406f1b99f3efdd1653641df75fdc7
jonathan010603 <jonathan010603@gmail.com>
2022-12-07 14:49:02 -0300

    feat (Dropdown): working and responsive

--- components/Menu/Menu__MobileDropdown.tsx
+++ components/Menu/Menu__MobileDropdown.tsx
@@ -60,21 +60,40 @@ const Menu__MobileDropdown = ({ open, setMobileDropdown }: IProps) => {
 };
 
 const Container = styled.div<IGetDropdownState>`
-  left: 0;
   top: 0;
-  z-index: 15;
+  left: 0;
   width: 100%;
-  display: ${(p) => (p.isOpen ? "flex" : "none")};
-  height: 95vh;
+  z-index: 15;
+  height: 60vh;
+  min-height: 340px;
   position: absolute;
-  flex-direction: column;
+  padding: 15vh 5.85vw 0 5.85vw;
+  backdrop-filter: blur(6px);
   background: linear-gradient(
     180deg,
     #020203 0%,
     rgb(14, 17, 23) 96.35%,
     rgb(14, 17, 23) 100%
   );
-  backdrop-filter: blur(6px);
+  justify-content: space-between;
+  display: ${(p) => (p.isOpen ? "flex" : "none")};
+
+  @media only screen and (min-width: 1201px) {
+    display: none;
+  }
+
+  @media only screen and (max-width: 420px) and (min-height: 600px) {
+    flex-direction: column;
+    height: 90vh !important;
+  }
+
+  @media only screen and (min-width: 376px) {
+    padding: 20vh 5.85vw 0 5.85vw;
+  }
+
+  @media only screen and (max-height: 690px) {
+    height: 50vh;
+  }
 
   ul {
     list-style: none;
@@ -82,19 +101,77 @@ const Container = styled.div<IGetDropdownState>`
 `;
 
 const Menu__MobileStdUl = styled.ul`
-  margin-top: 15vh;
-  text-align: center;
+  text-align: left;
 
   li {
-    font-size: 25px;
+    font-size: 35px;
     margin-bottom: 10px;
+
+    @media only screen and (max-height: 720px) {
+      font-size: 30px;
+    }
+
+    @media only screen and (max-height: 690px) {
+      font-size: 25px;
+    }
+
+    @media only screen and (max-width: 420px) {
+      font-size: 40px !important;
+      margin-bottom: 20px;
+    }
+
+    @media only screen and (max-width: 375px) {
+      font-size: 25px !important;
+      margin-bottom: 20px;
+    }
+
+    @media only screen and (max-width: 640px) {
+      font-size: 21px;
+    }
+
+    @media only screen and (max-width: 325px) {
+      font-size: 18px;
+    }
+
+    &:hover {
+      color: #00aeff;
+    }
   }
 `;
 
 const Menu__MobileGamesUl = styled.ul`
-  font-size: 15px;
-  margin-top: 30px;
-  padding-left: 5.85vw;
+  font-size: 25px;
+  padding: 10px 0 0 5.85vw;
+
+  @media only screen and (max-height: 720px) {
+    font-size: 20px;
+  }
+
+  @media only screen and (max-height: 690px) {
+    font-size: 15px;
+  }
+
+  @media only screen and (max-width: 420px) {
+    padding-left: 0;
+    font-size: 20px !important;
+  }
+
+  @media only screen and (max-width: 420px) {
+    padding-left: 0;
+    font-size: 16px !important;
+  }
+
+  @media only screen and (max-width: 640px) {
+    font-size: 15px;
+  }
+
+  @media only screen and (max-width: 401px) {
+    font-size: 11px;
+  }
+
+  @media only screen and (max-width: 325px) {
+    font-size: 10px;
+  }
 
   img {
     margin-right: 4vw;
@@ -102,19 +179,24 @@ const Menu__MobileGamesUl = styled.ul`
 
   li {
     margin-bottom: 20px;
+
+    @media only screen and (max-height: 690px) {
+      margin-bottom: 15px;
+    }
+
+    @media only screen and (max-width: 420px) {
+      margin-bottom: 30px;
+    }
+
+    &:hover {
+      color: #00aeff;
+    }
   }
 
   .seeAllGames {
     width: 15px;
     margin-left: 7px;
   }
-`;
-
-const Menu__MobileBlueLine = styled.div`
-  width: 90%;
-  height: 1px;
-  margin: 35px auto 20px auto;
-  background: #00aeff;
 `;
 
 export default Menu__MobileDropdown;
