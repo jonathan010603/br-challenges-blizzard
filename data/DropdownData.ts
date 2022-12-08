66585f81668e882e262608ae0664bb98e47ae52a
jonathan010603 <jonathan010603@gmail.com>
2022-12-08 18:31:26 -0300

    test (webp images)

--- data/DropdownData.ts
+++ data/DropdownData.ts
@@ -1,31 +1,34 @@
 export const DropdownGames = [
-  { name: "Diablo® II ressurected", logo: "/assets/icons/diablo2-icon.png" },
-  { name: "Overwatch® 2", logo: "/assets/icons/overwatch2-icon.png" },
-  { name: "World of Warcraft®", logo: "/assets/icons/wow-icon.png" },
-  { name: "Hearthstone®", logo: "/assets/icons/hearthstone-icon.png" },
-  { name: "Heroes of the storm®", logo: "/assets/icons/hots-icon.png" },
-  { name: "Warcraft® III Reforged", logo: "/assets/icons/warcraft3-icon.png" },
-  { name: "Diablo® IV", logo: "/assets/icons/diablo4-icon.png" },
-  { name: "Diablo® Immortal", logo: "/assets/icons/diabloImmortal-icon.png" },
-  { name: "Diablo® III", logo: "/assets/icons/diablo3-icon.png" },
-  { name: "StarCraft® II", logo: "/assets/icons/sc2-icon.png" },
-  { name: "StarCraft® Remastered", logo: "/assets/icons/sc-icon.png" },
-  { name: "Arcade da Blizzard®", logo: "/assets/icons/arcade-icon.png" },
+  { name: "Diablo® II ressurected", logo: "/assets/icons/diablo2-icon.webp" },
+  { name: "Overwatch® 2", logo: "/assets/icons/overwatch2-icon.webp" },
+  { name: "World of Warcraft®", logo: "/assets/icons/wow-icon.webp" },
+  { name: "Hearthstone®", logo: "/assets/icons/hearthstone-icon.webp" },
+  { name: "Heroes of the storm®", logo: "/assets/icons/hots-icon.webp" },
+  { name: "Warcraft® III Reforged", logo: "/assets/icons/warcraft3-icon.webp" },
+  { name: "Diablo® IV", logo: "/assets/icons/diablo4-icon.webp" },
+  { name: "Diablo® Immortal", logo: "/assets/icons/diabloImmortal-icon.webp" },
+  { name: "Diablo® III", logo: "/assets/icons/diablo3-icon.webp" },
+  { name: "StarCraft® II", logo: "/assets/icons/sc2-icon.webp" },
+  { name: "StarCraft® Remastered", logo: "/assets/icons/sc-icon.webp" },
+  { name: "Arcade da Blizzard®", logo: "/assets/icons/arcade-icon.webp" },
 ];
 
 export const DropdownSports = [
-  { name: "Hearthstone®\nmasters", logo: "/assets/sports/hearthstone-sports.png" },
+  {
+    name: "Hearthstone®\nmasters",
+    logo: "/assets/sports/hearthstone-sports.webp",
+  },
   {
     name: "Campeonato Mundial de\nArena WoW®",
-    logo: "/assets/sports/arenaWow-sports.png",
+    logo: "/assets/sports/arenaWow-sports.webp",
   },
-  { name: "StarCraft® II\nWCS", logo: "/assets/sports/sc2-sports.png" },
+  { name: "StarCraft® II\nWCS", logo: "/assets/sports/sc2-sports.webp" },
   {
     name: "Copa Mundial\nde Overwatch®",
-    logo: "/assets/sports/overwatchMundial-sports.png",
+    logo: "/assets/sports/overwatchMundial-sports.webp",
   },
   {
     name: "Liga de\nOverwatch®",
-    logo: "/assets/sports/overwatchLeague-sports.png",
+    logo: "/assets/sports/overwatchLeague-sports.webp",
   },
 ];
